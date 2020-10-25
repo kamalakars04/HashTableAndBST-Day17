f2d9e1ae2aab2615b41c3c2b4474e118ecf92087
kamalakars04 <kamalakars04@gmail.com>
2020-10-25 23:13:53 +0530

    [Kamalakar].[Refactor].Added Comments

--- BinarySearchTree.cs
+++ BinarySearchTree.cs
@@ -80,18 +80,25 @@ namespace HashTableAndBST
         public int Size()
         {
             int count = 0;
+
+            // If the tree is empty
             if(this.rootNode == null)
             {
                 return 0;
             }
+
+            // Iterate to the all left node of tree
             if(rootNode.leftTree !=null)
             {
                 count += rootNode.leftTree.Size();
             }
+
+            // Iterate to all right nodes of tree 
             if(rootNode.rightTree != null)
             {
                 count += rootNode.rightTree.Size();
             }
+
             count++;
             return count;
         }
